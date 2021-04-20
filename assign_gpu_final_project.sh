497f0253d72426008bf1603efbd8d125c6b246e9
Mangoman <1165268303@qq.com>
2021-04-19 20:35:15 -0400

    add assign gpu

---
+++ assign_gpu_final_project.sh
@@ -0,0 +1,7 @@
+# assign_gpu.sh
+
+# source /course/cs146/public/cs146-env/bin/activate
+source /home/dhuo/cs146_venv/bin/activate
+# source /course/cs146/public/cs146-gpu-new
+
+python3 lm_main.py -eTt
