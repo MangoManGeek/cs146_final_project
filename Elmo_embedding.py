83a2561d6eb90d216d349efbdbcc5e654c066f76
Mangoman <1165268303@qq.com>
2021-04-20 23:20:03 -0400

    require grad false

--- Elmo_embedding.py
+++ Elmo_embedding.py
@@ -9,7 +9,7 @@ class Elmo_Embedding_layer(nn.Module):
         # weight_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x1024_128_2048cnn_1xhighway/elmo_2x1024_128_2048cnn_1xhighway_weights.hdf5"
         # options_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x1024_128_2048cnn_1xhighway/elmo_2x1024_128_2048cnn_1xhighway_options.json"
         
-        self.elmo = Elmo(options_file, weight_file, 1, dropout=0, requires_grad = True)
+        self.elmo = Elmo(options_file, weight_file, 1, dropout=0, requires_grad = False)
 
         self.hidden_size = 1024
         # self.hidden_size = 256
